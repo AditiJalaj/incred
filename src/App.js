c4bbfb7b29d9c3640b1bdb7d9a2c7fa07dd6337f
ajalaj <ajalaj@AJALAJ-IN.oradev.oraclecorp.com>
2021-05-11 14:32:51 +0530

    facebook login added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import FacebookLogin from 'react-facebook-login';
+import { Card, Image } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const App=()=>{
+
+  const [login,setLogin]=useState(false)
+  const [data,setData]=useState({})
+  const [picture,setPicture]=useState('')
+
+  const responseFacebook=(res)=>{
+    console.log(res)
+    setData(res)
+    //setPicture(res.picture.data.url)
+    if(res.accessToken){
+      setLogin(true)
+    }
+    else {
+      setLogin(false)
+    }
+  }
+
+  return(
+
+    <div className="container">
+    <Card style={{width:'600px'}}>
+    <Card.Header>
+    { 
+    <FacebookLogin
+    appId='850425218886243'
+    callback={responseFacebook}
+    autoLoad={true}
+    fields="name,email,picture"
+    scope="public_profile, email"
+    icon='fa-facebook'
+    /> }
+
+    { login && <Image src={picture} roundedCircle/>}
+    </Card.Header>
+    {login &&
+    <Card.Body>
+    <Card.Title>{data.name}</Card.Title>
+    <Card.Text>{data.email}</Card.Text>
+    </Card.Body>}
+    </Card>
     </div>
-  );
+    
+    )
 }
 
 export default App;
